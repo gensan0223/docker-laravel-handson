cfc104130a3ec6397ee1cd14cb438ad4ce7c0dde
弦間由樹 <gemma.naok@gmail.com>
2021-03-15 23:56:01 +0900

    DAWN CREATEの実装終了

--- backend/resources/views/posts/index.blade.php
+++ backend/resources/views/posts/index.blade.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html>
- 
+
 <head>
     <meta charset='utf-8"'>
     <link rel='stylesheet' href="{{ asset('/css/app.css') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
- 
+
 <body>
- 
+
     <header>
         <h1>Laravelを使った投稿機能の実装</h1>
     </header>
- 
+
     <div class='container'>
+        <p class="pull-right"><a class="btn btn-success" href="post/createForm">投稿する</a></p>
         <h2 class='page-header'>投稿一覧</h2>
         <table class='table table-hover'>
             <tr>
@@ -34,7 +35,6 @@
         <small>Laravel@dawn.curriculum</small>
     </footer>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
- 
+
 </body>
- 
 </html>
